3c878f1e3512558a3ca05053b1ec110ffd3d1231
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-07-02 18:32:50 +0600

    fix navbar test

--- test/hermione/menu.hermione.ts
+++ test/hermione/menu.hermione.ts
@@ -1,8 +1,13 @@
-describe('navbar on less than 576', function () {
+describe('menu on less than 576', function () {
   it('is hidden', async function () {
     await this.browser.url('http://localhost:3000/hw/store')
+
     this.browser.setWindowSize(570, (await this.browser.getWindowSize()).height)
-    await this.browser.assertView('plain', '.navbar')
+
+    const navbar = await this.browser.$('.Application-Menu')
+    const isNavbarDisplayed = await navbar.isDisplayed()
+
+    expect(isNavbarDisplayed).toBeFalsy()
   })
 
   it('closes after link is clicked', async function () {
@@ -15,6 +20,9 @@ describe('navbar on less than 576', function () {
     const link = await this.browser.$('.nav-link')
     await link.click()
 
-    await this.browser.assertView('plain', '.navbar')
+    const navbar = await this.browser.$('.Application-Menu')
+    const isNavbarDisplayed = await navbar.isDisplayed()
+
+    expect(isNavbarDisplayed).toBeFalsy()
   })
 })
